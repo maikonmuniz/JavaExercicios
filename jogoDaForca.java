3740ee27e814ac3533cc677ee09e9e3cb2ae3477
maikonmuniz <maikon.muniz@yahoo.com.br>
2022-11-15 18:51:47 -0300

    project game completed

--- jogoDaForca.java
+++ jogoDaForca.java
@@ -1,36 +1,101 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Vector;
 
-public class jogoDaForca {
+public class JogoDaForca {
     public static void main(String[] args){
 
         Scanner letters = new Scanner(System.in);
 
+        ArrayList<String> errLetter = new ArrayList<>();
+
         System.out.println("Digite a palavra");
+        String forca              = letters.nextLine();
 
-
-        String forca = letters.nextLine();
         String[] forcaArrayLetter = forca.split("");
+        // String[] errLetter        = new String[6];
+        String letterHiden        = listHideLetter(forca.length());
+        String[] vetorLetterHiden = letterHiden.split(" ");
+        Boolean game              = true;
+        int err                   = 0;
 
-
-        String letterHiden = listHideLetter(forca.length());
-
-        Boolean game = true;
-        int count    = 0;
-
-        do{
+        do {
 
             System.out.println("Digite uma Letra:");
-            char c = letters.next().charAt(0);
+            String c = letters.nextLine().substring(0);
 
-            count += 1;
-            System.out.println(c);
+            if(checkLetter(c, forca)){
+                vetorLetterHiden = countLetterInVetor(c, forcaArrayLetter, vetorLetterHiden);
+            }
+            else {
+                errLetter.add(c);
+                err       += 1;
+            }
+            
 
-            if(count == 6){
+            System.out.println("-----------------------------------------------------");
+
+                mistake(err);
+
+            System.out.println("-----------------------------------------------------");
+
+            System.out.println("Letras errados:  "+String.join(" ", errLetter));
+
+            System.out.println("-----------------------------------------------------");
+        
+            System.out.println("Letras certas: "+String.join(" ", vetorLetterHiden));
+
+            System.out.println("-----------------------------------------------------");
+
+            game = completeLetters(vetorLetterHiden);
+
+            if(err == 6){
+                System.out.println("-----------------------------------------------------");
+                System.out.println("-----------------------------------------------------");
+                System.out.println("-----------------------------------------------------");
+                System.out.println("------------------[Game Over!]-----------------------");
+                System.out.println("-----------------------------------------------------");
+                System.out.println("-----------------------------------------------------");
+                System.out.println("-----------------------------------------------------");
                 game = false;
             }
 
         } while(game);
+    }
+
+    public static Boolean completeLetters(String[] vetorLetterHiden){
+
+        String LetterExistHiden = String.join(" ", vetorLetterHiden);
+
+        if(LetterExistHiden.contains("_")){
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void mistake(int err){
+
+        String[] errVetor = {" O\n", "/", "I", "\\ \n", "/ ", "\\"};
+
+        if (err == 1){
+            System.out.println(errVetor[0]);
+        }
+        else if (err == 2){
+            System.out.println(errVetor[0]+errVetor[1]);
+        }
+        else if (err == 3){
+            System.out.println(errVetor[0]+errVetor[1]+errVetor[2]);
+        }
+        else if (err == 4){
+            System.out.println(errVetor[0]+errVetor[1]+errVetor[2]+errVetor[3]);
+        }
+        else if (err == 5){
+            System.out.println(errVetor[0]+errVetor[1]+errVetor[2]+errVetor[3]+errVetor[4]);
+        }
+        else if (err == 6){
+            System.out.println(errVetor[0]+errVetor[1]+errVetor[2]+errVetor[3]+errVetor[4]+errVetor[5]);
+        }
     }
 
     public static String listHideLetter(int qtdChar){
@@ -44,7 +109,29 @@ public class jogoDaForca {
         return String.join(" ", ArrayHide);
     }
 
-    // public static String checkLetter(String letter, String[] word){
-        
-    // }
+    public static Boolean checkLetter(String letter, String word){
+
+        if(word.contains(letter)){
+            return true;
+        }
+
+        return false;
+    }
+
+    public static String[] countLetterInVetor(String letter, String[] word, String[] letterHiden){
+
+        int count = 0;
+
+        for(String i: word){
+            
+            if(i.equals(letter)){
+
+                letterHiden[count] = letter;
+
+            }
+            count += 1;
+        }
+
+        return letterHiden;
+    }
 }
